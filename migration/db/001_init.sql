3bc97bdce74f81e8d879a2069e1c7a1f606b16a8
azuki774s <azuki774s@gmail.com>
2023-09-16 23:50:40 +0900

    add initial sql

---
+++ migration/db/001_init.sql
@@ -0,0 +1,29 @@
+-- +migrate Up
+
+CREATE TABLE `detail` (
+  `id` INT NOT NULL AUTO_INCREMENT comment 'primary id',
+  `yyyymm_id` INT NOT NULL comment 'id for each yyyymm',
+  `date` DATE NOT NULL comment 'record date yyyymm',
+  `name` TEXT comment 'detail name',
+  `price` INT,
+  `fin_ins` TEXT comment 'finance instrcument name',
+  `l_category` TEXT comment 'large category name',
+  `m_category` TEXT comment 'medium category name',
+  `regist_date` DATE NOT NULL comment 'date running importer',
+  `maw_check_date` DATE comment 'mawinter check date',
+  `maw_regist_date` DATE comment 'mawinter regist check date',
+  `raw_date` TEXT,
+  `raw_price` TEXT,
+  `created_at` datetime  default current_timestamp,
+  `updated_at` timestamp default current_timestamp on update current_timestamp,
+  PRIMARY KEY (`id`),
+  INDEX `idx1` (`maw_check_date`),
+  INDEX `idx2` (`name`),
+  INDEX `idx3` (`raw_price`)
+);
+
+
+
+-- +migrate Down
+
+DROP TABLE `detail`;
