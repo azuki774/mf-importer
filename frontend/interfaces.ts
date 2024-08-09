8608ff0c27e59a4857aecd63a296722f3b4b1c6b
azuki774s <azuki774s@gmail.com>
2024-08-10 02:21:43 +0900

    first contents from fixed endpoint

---
+++ frontend/interfaces.ts
@@ -0,0 +1,7 @@
+export interface ImportRecord {
+    use_date: string;
+    name: string;
+    regist_date: string;
+    import_judge_date: string;
+    import_date: string;
+}
---
+++ frontend/pages/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { ImportRecord } from "@/interfaces";
+const record_list = ref<ImportRecord[]>()
+const response = await $fetch("http://172.19.250.172:20010/") as ImportRecord[]; // TODO: アドレス
+record_list.value = response // APIから取得した値を代入
+</script>
+
+<template>
+  <h1>mf-importer-web</h1>
+
+  <section class="container">
+    <h1>リスト</h1>
+    <table class="Lists">
+      <thead>
+        <tr>
+          <th>利用日時</th>
+          <th>名前</th>
+          <th>登録日時</th>
+          <th>取り込み判定日時</th>
+          <th>取り込み日時</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="record in record_list" :key="record_list">
+          <td>{{ record.use_date }}</td>
+          <td>{{ record.name }}</td>
+          <td>{{ record.regist_date }}</td>
+          <td>{{ record.import_judge_date }}</td>
+          <td>{{ record.import_date }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
