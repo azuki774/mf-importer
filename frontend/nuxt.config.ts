6e5af04eff014d6aed7abd818eb6bf605ab33d01
azuki774s <azuki774s@gmail.com>
2024-08-22 11:48:19 +0000

    install bootstrap

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  css: ["bootstrap/dist/css/bootstrap.min.css"],
   routeRules: {
     "/": { ssr: true },
   },
