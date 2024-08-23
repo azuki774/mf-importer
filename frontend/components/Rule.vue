383b94cef723ded76fd40581ec3c7038032fddea
azuki774s <azuki774s@gmail.com>
2024-08-24 02:52:41 +0900

    add POST rule from window

--- frontend/components/Rule.vue
+++ frontend/components/Rule.vue
@@ -9,14 +9,84 @@ const asyncData = await useFetch(
   }
 );
 
-const data = asyncData.data.value as Rule[];
-rule_list.value = data;
+onMounted(
+  (): void => {
+    const data = asyncData.data.value as Rule[];
+    if (data != undefined) {
+      rule_list.value = data;
+    }
+  }
+)
+
+
+const postButton = async (): Promise<void> => {
+  const param = { 'fieldName': ruleFieldSelector.value, 'value': ruleFilterValue.value, 'categoryId': ruleCategoryID.value, 'exactMatch': ruleExactCheck.checked }
+  // exactMatch: true -> 1, false -> 0
+  if (param['exactMatch'] == true) {
+    param['exactMatch'] = 1;
+  } else {
+    param['exactMatch'] = 0;
+  }
+
+  const localurl = "/api/rules"
+  // create body
+  const reqbody = `
+  {
+  "fieldName":"${param.fieldName}",
+  "value":"${param.value}",
+  "categoryId":${param.categoryId},
+  "exactMatch":${param.exactMatch}
+  }
+  `
+  console.log(reqbody)
+  const result = await $fetch(localurl,
+    {
+      method: "POST",
+      body: reqbody
+    }
+  )
+
+  location.reload()
+};
 
 </script>
 
 <template>
   <section class="container">
     <h3>抽出連携ルール</h3>
+
+    <h4>追加</h4>
+    <div class="mb-2">
+      <select class="form-select" id="ruleFieldSelector">
+        <option value="name" selected>name</option>
+        <option value="m_category">m_category</option>
+      </select>
+    </div>
+
+    <div class="mb-3">
+      <label class="form-label">フィルタ値</label>
+      <input class="form-control" id="ruleFilterValue">
+    </div>
+
+    <div class="mb-1">
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input" value="" id="ruleExactCheck">
+        <label for="formCheckDefault" class="form-check-label">
+          完全一致
+        </label>
+      </div>
+    </div>
+
+    <div class="mb-3">
+      <label class="form-label">カテゴリID</label>
+      <input type="number" class="form-control" id="ruleCategoryID">
+    </div>
+
+    <div class="mb-3">
+      <button type="submit" @click="postButton" name="postButton" class="btn btn-primary">追加する</button>
+    </div>
+
+    <h4>一覧</h4>
     <table class="table small bordered striped table-bordered">
       <thead class="table-warning">
         <tr>
